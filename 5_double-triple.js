d10f532e554ab9ac572099c08182622ace21562e
noman-swe <noman.swe130@gmail.com>
2022-03-06 00:56:38 +0600

    35-5 double equal (==) vs triple equal (===), implicit conversion

---
+++ 5_double-triple.js
@@ -0,0 +1,15 @@
+const first = 2;
+const secoond = '2';
+if(first === secoond){
+    console.log('Everything Equal');
+}
+else{
+    console.log('nothing is equal');
+}
+/* 
+double equal checks the value is equal or not but === checks the value is equal or not & also the type of the elements.
+
+
+....
+primitive er khet-tre == diye check kora jay but non-primitive(object, array) er khet-tre double equal diye check kora possible na
+*/
